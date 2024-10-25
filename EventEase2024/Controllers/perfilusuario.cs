62c7f7fb79b478bc6eb019894d59dfb00cf3f645
hudson martins <hudsonlh.martins@gmail.com>
2024-10-25 19:55:31 -0300

    remove folder git ignore

---
+++ EventEase2024/Controllers/perfilusuario.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventEase2024.Controllers;
+
+public class perfilusuario : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Page1()
+    {
+        return View();
+    }
+}
